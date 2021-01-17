12d84b7af89800812a52c82fbd64deab5d6711a2
Luiz Henrique Oliveira <luizhenrique3574@gmail.com>
2021-01-17 12:12:20 -0300

    Configurando e usando o Entity Framework

--- QuickBuy.Dominio/Entidades/Usuario.cs
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -11,7 +11,11 @@ namespace QuickBuy.Dominio.Entidades
         public string Senha { get; set; }
         public string Nome { get; set; }
         public string SobreNome { get; set; }
-        public ICollection<Pedido> Pedidos { get; set; }
+
+        /// <summary>
+        /// Um usuario pode ter nenhum ou muitos pedidos
+        /// </summary>
+        public virtual ICollection<Pedido> Pedidos { get; set; }
 
         public override void Validate()
         {
