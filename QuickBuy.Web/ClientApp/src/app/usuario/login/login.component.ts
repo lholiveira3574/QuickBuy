c7946ce5d700c7826b32d13d76e76b6a8bb9c8d1
Luiz Henrique Oliveira <luizhenrique3574@gmail.com>
2021-01-24 21:34:42 -0300

    Angular - Estrutura e Template

---
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from "@angular/core";
+import { Usuario } from "../../modelo/usuario";
+import { ActivatedRoute, Router } from "@angular/router";
+
+@Component({
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
+})
+
+export class LoginComponent implements OnInit {
+  public usuario;
+  public returnUrl: string;
+
+  constructor(private router: Router, private activatedRouter: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.returnUrl = this.activatedRouter.snapshot.queryParams['returnUrl'];
+    this.usuario = new Usuario();
+  }
+
+  entrar() {
+    if (this.usuario.email == "teste@teste.com" && this.usuario.senha == "123") {
+      sessionStorage.setItem("usuario-autenticado", "1");
+      this.router.navigate([this.returnUrl]);
+    }
+  }
+
+}
